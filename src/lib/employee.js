cb972e2591194b948edfe963c54dc7f390ea518d
Tudor Tocan <tudor.tocan@gmail.com>
2021-05-11 12:24:08 +0100

    add methods to Employee.js

--- src/lib/employee.js
+++ src/lib/employee.js
@@ -5,6 +5,22 @@ class Employee {
     this.email = email;
     this.role = role;
   }
+
+  getName() {
+    return this.name;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getEmail() {
+    return this.email;
+  }
+
+  getRole() {
+    return this.role;
+  }
 }
 
 module.exports = Employee;
