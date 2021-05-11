6f61059b13e901b7824a5aabea0a636c0bef43dc
Tudor Tocan <tudor.tocan@gmail.com>
2021-05-11 16:47:40 +0100

    add code to index.js to ask mock questions and console.log the answers

--- src/index.js
+++ src/index.js
@@ -9,9 +9,30 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
+const questions = [
+  {
+    type: "input",
+    message:
+      "Please say hello Please say hello Please say hello Please say hello",
+    name: "hello",
+  },
+
+  {
+    type: "input",
+    message:
+      "Please say goodbye Please say goodbye Please say goodbye Please say goodbye",
+    name: "goodbye",
+  },
+];
+
 //this will initialize the app
-const init = () => {
-  console.log("App Initialized");
+const init = async () => {
+  try {
+    const answers = await inquirer.prompt(questions);
+    console.log(answers);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 init();
